be6c308e992eb5a5cb1b21822d2b740c549564d4
Samuel Mejía <150869146+Biggles-insano@users.noreply.github.com>
2024-05-01 09:10:21 -0600

    Update Arbol.java

--- compresión/Arbol.java
+++ compresión/Arbol.java
@@ -8,11 +8,34 @@ public class Arbol {
     }
 
     public void generarCodigos(HashMap<Character, String> codigos, String codigoActual) {
-        if (raiz.izquierda == null && raiz.derecha == null) {
-            codigos.put(raiz.caracter, codigoActual);
+        generarCodigosRecursivo(raiz, codigos, codigoActual);
+    }
+
+    private void generarCodigosRecursivo(Nodo nodo, HashMap<Character, String> codigos, String codigoActual) {
+        if (nodo.izquierda == null && nodo.derecha == null) {
+            codigos.put(nodo.caracter, codigoActual);
         } else {
-            raiz.izquierda.generarCodigos(codigos, codigoActual + "0");
-            raiz.derecha.generarCodigos(codigos, codigoActual + "1");
+            generarCodigosRecursivo(nodo.izquierda, codigos, codigoActual + "0");
+            generarCodigosRecursivo(nodo.derecha, codigos, codigoActual + "1");
+        }
+    }
+
+    public void imprimirArbol() {
+        imprimirArbolRecursivo(raiz, 0);
+    }
+
+    private void imprimirArbolRecursivo(Nodo nodo, int nivel) {
+        if (nodo != null) {
+            imprimirArbolRecursivo(nodo.derecha, nivel + 1);
+            for (int i = 0; i < nivel; i++) {
+                System.out.print("  ");
+            }
+            if (nodo.caracter == '\0') {
+                System.out.println("Frecuencia: " + nodo.frecuencia);
+            } else {
+                System.out.println("Caracter: " + nodo.caracter + " Frecuencia: " + nodo.frecuencia);
+            }
+            imprimirArbolRecursivo(nodo.izquierda, nivel + 1);
         }
     }
 }
